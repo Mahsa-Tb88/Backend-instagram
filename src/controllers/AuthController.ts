8041f1c2c5ceeceeb44cfbbbe6ddcc9ac27730ea
Mahsa <50694999+Mah-tab88@users.noreply.github.com>
2025-08-09 22:31:36 -0700

    add login route

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,9 +1,13 @@
-import { Request, Response } from "express";
+import { CookieOptions, Request, Response } from "express";
 import User from "../models/UserModels.js";
 import { sendActivationMail } from "../utils/mailing.js";
 
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+
 export default class AuthController {
   static async register(req: RegisterRequest, res: Response) {
+    console.log("yees");
     const { username, password, email, fullname } = req.body;
     const activationCode = Math.floor(Math.random() * 900000 + 100000);
     const user = await User.create({
@@ -33,12 +37,46 @@ export default class AuthController {
         return res.fail("Invalid Username or activation code");
       }
       user.activationCode = 0;
-      user.save();
+      await user.save();
       res.success("User activated Successfully!");
     } else {
       res.fail("Invalid Request");
     }
   }
+
+  static async login(req: LoginRequest, res: Response) {
+    const { username, password, remember } = req.body;
+    if (!username || !password) {
+      return res.fail("Please enter username and password!");
+    }
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.fail("Username or password is not valid!");
+    }
+
+    const match = bcrypt.compare(password, user.password!);
+    if (!match) {
+      return res.fail("Username or password is not valid!");
+    }
+
+    if (user.activationCode) {
+      return res.fail("User is not activated!");
+    }
+
+    const token = jwt.sign({ id: user._id }, SECRET_KEY, { expiresIn: remember ? "7d" : "4h" });
+
+    const settings: CookieOptions = {
+      httpOnly: true,
+      secure: true,
+      sameSite: "lax",
+    };
+    if (remember) {
+      settings.maxAge = 7 * 86400 * 1000;
+    }
+    res.cookie("token", token, settings);
+    res.success("user logined successfully!", user);
+  }
+  static async logou(req: LoginRequest, res: Response) {}
 }
 
 type RegisterRequest = Request<
@@ -48,3 +86,4 @@ type RegisterRequest = Request<
 >;
 
 type ActivateReques = Request<any, any, { username: string; activationCode: number }>;
+type LoginRequest = Request<any, any, { username: string; password: string; remember?: boolean }>;
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -4,5 +4,8 @@ import AuthController from "../controllers/AuthController.js";
 const router = express.Router();
 
 router.post("/register", AuthController.register);
+router.post("/activate", AuthController.activate);
+router.post("/login", AuthController.activate);
+router.post("/logout", AuthController.activate);
 
 export default router;
